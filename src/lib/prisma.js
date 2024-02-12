f5c000e978d234763dead7ba0c8a64e02fe7ff53
shunmakk <shunmakajioka12@gmail.com>
2024-02-12 10:12:43 +0900

    準備

---
+++ src/lib/prisma.js
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+//prismaのクライアントオブジェクトを設定する
+
+//global.prisma上にprismaクライアントが存在すれば再利用
+const prisma = global.prisma ?? new PrismaClient({log : ['query']});
+
+//非production環境では、global.prismaにオブジェクトを格納
+if(process.env.NODE_ENV !== 'production') global.prisma = prisma
+
+export default prisma;
